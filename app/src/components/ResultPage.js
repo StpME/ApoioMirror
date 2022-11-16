0ae541fba5dd1c4685224ba8acc312560d621559
ayatab <42857860+ayatab@users.noreply.github.com>
2022-11-16 14:10:24 -0800

    added components and the page for results page

---
+++ app/src/components/ResultPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ResultFilter } from './ResultFilter';
+import { ResultItem } from './ResultItem';
+
+export default function ResultPage(props) {
+    return (
+        <div className="container mt-5">
+            <div className="row m-auto">
+                <div className="col-md-4 col-lg-3">
+                    <ResultFilter />
+                </div>
+                <div className="col-md-6 col-lg-9 flex-column">
+                    <ResultItem />
+                </div>
+            </div>
+        </div>
+    );
+}
