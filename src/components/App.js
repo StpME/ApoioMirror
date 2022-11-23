21a7db4056cf25552563c19591074869842d90cc
Justin Tham <justinctham@hotmail.com>
2022-11-23 02:41:04 -0800

    Added home component and bootstrap stylesheet to index.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 import {ListCard} from './ListPage.js';
 import {CreateList} from './ListPage.js';
+import Home from './Home.js';
 
 function App(props) {
     const stores = props.stores;
@@ -10,7 +11,8 @@ function App(props) {
     return (
         <div>
             <Navbar />
-            <ListCard stores={stores}/>
+            <Home />
+            {/* <ListCard stores={stores}/> */}
             
 
             <Footer />
