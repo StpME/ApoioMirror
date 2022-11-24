f48c7eee30e09cce417b4e474bd0e39f9737ec1f
Justin Tham <justinctham@hotmail.com>
2022-11-23 19:56:42 -0800

    added home page

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const CARD_CATEGORIES = [
+    {name:'Food', img:'pics/hamburger.png'},
+    {name:'Clothing', img:'pics/shirt.png'},
+    {name:'Groceries', img:'pics/basket.png'}    
+]
+
+export default function Home(props) {
+
+    const categoryCards = CARD_CATEGORIES.map((element) => {
+        return <CategoryCard img={element.img} name={element.name} />
+    })
+
+    return (
+        <main>
+            <div className="banner row">
+                <img src="pics/restaurant.png" alt="The front of the restuarant Sizzle and Crunch" />
+            </div>
+            <div className="row">
+                <h2 className="text-center p-3">Browse by category</h2>
+            </div>
+            <div className="row categories justify-content-center">
+                {categoryCards}
+            </div>
+        </main>
+    );
+}
+
+function CategoryCard(props) {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <img src={props.img} alt={props.name + " category"} />
+            </div>
+            <div className="card-title">
+                <a href="results.html" className="btn stretched-link">{props.name}</a>
+            </div>
+        </div>
+    );
+}
