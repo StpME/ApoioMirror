c06575ea52aefc4ab688c26423f003d05c0ad43a
Justin Tham <justinctham@hotmail.com>
2022-12-13 04:55:22 -0800

    changed home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,48 +12,50 @@ const CARD_CATEGORIES = [
 export default function Home(props) {
 
     const categoryCards = CARD_CATEGORIES.map((element, index) => {
-        return <CategoryCard key={index} typeShop={element.typeShop} img={element.img} name={element.name} typeStoreCallback={props.typeStoreCallback}/>
+        return <CategoryCard key={index} typeShop={element.typeShop} img={element.img} name={element.name} typeStoreCallback={props.typeStoreCallback} />
     })
 
-    
+
 
     return (
         <main>
-            <div className="banner row">
-                <Carousel className="slides">
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="pics/restaurant.png"
-                            alt="Sizzle and Crunch"
-                        />
-                        <Carousel.Caption>
-                            <Link className="text-decoration-none text-light" to="/results">
-                                <h3>Looking for food?</h3>
-                                <p>Try Sizzle and Crunch! A local fast-casual Vietnamese restaurant!</p>
-                            </Link>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src="pics/butter_home_tn_crop.jpg"
-                            alt="Butter Home"
-                        />
-                        <Carousel.Caption>
-                            <Link className="text-decoration-none text-light" to="/results">
-                                <h3>Bored?</h3>
-                                <p>Stop by Butter Home, a brick + mortor shop in Seattle specializing in fun and functional items for you and your home!</p>
-                            </Link>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </div>
-            <div className="row">
-                <strong><header className="text-center p-3" id="header">Browse by category</header></strong>
-            </div>
-            <div className="row pb-5 categories justify-content-center">
-                {categoryCards}
+            <div className="container-fluid">
+                <div className="banner row">
+                    <Carousel className="slides">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="pics/restaurant.png"
+                                alt="Sizzle and Crunch"
+                            />
+                            <Carousel.Caption>
+                                <Link className="text-decoration-none text-light" to="/results">
+                                    <h3>Looking for food?</h3>
+                                    <p>Try Sizzle and Crunch! A local fast-casual Vietnamese restaurant!</p>
+                                </Link>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="pics/butter_home_tn_crop.jpg"
+                                alt="Butter Home"
+                            />
+                            <Carousel.Caption>
+                                <Link className="text-decoration-none text-light" to="/results">
+                                    <h3>Bored?</h3>
+                                    <p>Stop by Butter Home, a brick + mortor shop in Seattle specializing in fun and functional items for you and your home!</p>
+                                </Link>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
+                <div className="row">
+                    <strong><header className="text-center p-3" id="header">Browse by category</header></strong>
+                </div>
+                <div className="row pb-5 categories justify-content-center">
+                    {categoryCards}
+                </div>
             </div>
         </main>
     );
